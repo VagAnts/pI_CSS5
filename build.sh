1653f8f278ecf95f4640dbad1bf26ecece9c3e9c
VagAnts <98564398+VagAnts@users.noreply.github.com>
2021-01-23 22:36:04 +0000

    ....I..... [ZBXNEXT-973] redesign of existing templates

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 GOOS=darwin go build -o bin/gocheckitout-mac
 GOOS=linux go build -o bin/gocheckitout-linux
-GOOS=windows go build -o bin/gocheckitout.ex
+GOOS=windows go build -o bin/gocheckitout.exe
