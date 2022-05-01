d99326d4c01b3dc0ba9644dda81b2514397dbab1
VagAnts <98564398+VagAnts@users.noreply.github.com>
2022-05-01 13:50:20 +0000

    Fixed condition

--- helpers/requester.go
+++ helpers/requester.go
@@ -18,4 +18,4 @@ func Requester(url string) (*utils.Custom, error) {
 
 	custom := utils.Custom{
 		Header:     res.Header,
-		StatusCode: int16(res.StatusCode)
+		StatusCode: int16(res.StatusCode),
