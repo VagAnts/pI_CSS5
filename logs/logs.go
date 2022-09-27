db07cc675d202d7aed7f7ccefd3b2b3e3c16e053
VagAnts <98564398+VagAnts@users.noreply.github.com>
2022-09-27 14:47:57 +0000

    - Improved documentation.

--- logs/logs.go
+++ logs/logs.go
@@ -7,3 +7,4 @@ func WelcomeLog() {
 	fmt.Println(
 		"Welcome to go Check-it-out, Choose an option to execute the features", "\n",
 		"1 - Only make a request with no details", "\n",
+		"2 - Make a request with some details", "\n",
