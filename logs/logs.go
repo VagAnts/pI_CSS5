1504da0bf0c1bbc17897ae65810cfb529e7a3875
VagAnts <98564398+VagAnts@users.noreply.github.com>
2022-09-28 07:01:29 +0000

    1.2.20

--- logs/logs.go
+++ logs/logs.go
@@ -8,3 +8,4 @@ func WelcomeLog() {
 		"Welcome to go Check-it-out, Choose an option to execute the features", "\n",
 		"1 - Only make a request with no details", "\n",
 		"2 - Make a request with some details", "\n",
+		"3 - For exit CLI menu",
