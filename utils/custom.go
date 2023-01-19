97cd949ff141bf10c3519aad2b6edb44a5e4289b
VagAnts <98564398+VagAnts@users.noreply.github.com>
2023-01-19 07:19:02 +0000

    Added blog

--- utils/custom.go
+++ utils/custom.go
@@ -11,3 +11,4 @@ import (
 // But remember that respose must be legit in according within http struct.
 type Custom struct {
 	Header     http.Header    // Header contents like content-type, accept, etc...
+	StatusCode int16          //output default status code - 200. (This app only returns 200 if OK, if don't will return the specified configurated status code from errors.)
